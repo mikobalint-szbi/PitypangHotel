eeac057725ff212fe40daa917569cdf10730ebf8
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2024-03-22 19:45:50 +0100

    Üdvözlő rész

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -58,6 +58,7 @@
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 1);
@@ -70,9 +71,11 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1036, 573);
             this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Visible = false;
             // 
             // tableLayoutPanel4
             // 
+            this.tableLayoutPanel4.BackColor = System.Drawing.Color.Transparent;
             this.tableLayoutPanel4.ColumnCount = 2;
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
@@ -85,7 +88,6 @@
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel4.Size = new System.Drawing.Size(1030, 507);
             this.tableLayoutPanel4.TabIndex = 1;
-            this.tableLayoutPanel4.Visible = false;
             // 
             // dataGridView1
             // 
@@ -103,7 +105,7 @@
             this.tableLayoutPanel5.BackColor = System.Drawing.Color.Gray;
             this.tableLayoutPanel5.ColumnCount = 3;
             this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 99F));
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 121F));
             this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 33F));
             this.tableLayoutPanel5.Controls.Add(this.label7, 1, 0);
             this.tableLayoutPanel5.Controls.Add(this.label6, 0, 0);
@@ -137,9 +139,9 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label7.Location = new System.Drawing.Point(41, 0);
+            this.label7.Location = new System.Drawing.Point(19, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(93, 1);
+            this.label7.Size = new System.Drawing.Size(115, 1);
             this.label7.TabIndex = 4;
             this.label7.Text = "label7";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -150,7 +152,7 @@
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label6.Location = new System.Drawing.Point(3, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(32, 1);
+            this.label6.Size = new System.Drawing.Size(10, 1);
             this.label6.TabIndex = 3;
             this.label6.Text = "label6";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -170,9 +172,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label5.Location = new System.Drawing.Point(41, -10);
+            this.label5.Location = new System.Drawing.Point(19, -10);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 32);
+            this.label5.Size = new System.Drawing.Size(115, 32);
             this.label5.TabIndex = 1;
             this.label5.Text = "Év";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -181,18 +183,18 @@
             // 
             this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(41, 25);
+            this.comboBox1.Location = new System.Drawing.Point(19, 25);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(93, 21);
+            this.comboBox1.Size = new System.Drawing.Size(115, 21);
             this.comboBox1.TabIndex = 2;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label8.Location = new System.Drawing.Point(41, 53);
+            this.label8.Location = new System.Drawing.Point(19, 53);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(93, 22);
+            this.label8.Size = new System.Drawing.Size(115, 22);
             this.label8.TabIndex = 5;
             this.label8.Text = "Szoba";
             this.label8.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
@@ -201,9 +203,9 @@
             // 
             this.comboBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(41, 78);
+            this.comboBox2.Location = new System.Drawing.Point(19, 78);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(93, 21);
+            this.comboBox2.Size = new System.Drawing.Size(115, 21);
             this.comboBox2.TabIndex = 6;
             // 
             // panel1
@@ -268,7 +270,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(383, 0);
             this.label2.Name = "label2";
@@ -308,6 +310,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1036, 573);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
--- Form1.cs
+++ Form1.cs
@@ -17,36 +17,107 @@ namespace PitypangHotel
 
         public Form1()
         {
+            
             InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
 
             tableLayoutPanel2.BackColor = Color.FromArgb(0, 127, 115);
             tableLayoutPanel1.BackColor = Color.FromArgb(255, 255, 255);
-
             tableLayoutPanel5.BackColor = Color.FromArgb(0, 127, 115);
 
-            var pfc = new PrivateFontCollection();
-            pfc.AddFontFile("Belanosima-Regular.ttf"); 
-            label1.Font = new Font(pfc.Families[0], 14, FontStyle.Regular);
 
-            //label6.Font = new Font(pfc.Families[0], 13, FontStyle.Regular);
-
+            var betuTipus = new PrivateFontCollection();
+            betuTipus.AddFontFile("Belanosima-Regular.ttf"); 
+            label1.Font = new Font(betuTipus.Families[0], 14, FontStyle.Regular);
 
 
         }
 
+        int unit = 1;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            this.BackColor = Color.FromArgb(0, 127, 115);
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            Label nagySzoveg = new Label()
+;           this.Controls.Add(nagySzoveg);
+
+            nagySzoveg.Top = 120;
+            nagySzoveg.Left = 120;
+            unit = unit + 1;
+
+            nagySzoveg.ForeColor = Color.White;
+
+            var betuTipus = new PrivateFontCollection();
+            betuTipus.AddFontFile("Belanosima-Regular.ttf");
+            nagySzoveg.Font = new Font(betuTipus.Families[0], 46, FontStyle.Regular);
+
+
+            nagySzoveg.Size = new Size(800, 180);
+            nagySzoveg.TextAlign = ContentAlignment.MiddleCenter;
+            nagySzoveg.Text = "Üdvözlünk a Pitypang Hotel asztali alkalmazásában";
+
+
+
+            Button foglalas = new Button();
+            foglalas.Height = 50;
+            foglalas.Width = 120;
+            //foglalas.BackColor = Color.Red;
+            foglalas.ForeColor = Color.White;
+            foglalas.Location = new Point(360, 300);
+            foglalas.Text = "Foglalás";
+            foglalas.Name = "foglalasButton";
+            foglalas.Font = new Font(betuTipus.Families[0], 14, FontStyle.Regular);
+            foglalas.Click += new EventHandler(foglalasButton_Click);
+            Controls.Add(foglalas);
+
+            Button stat = new Button();
+            stat.Height = 50;
+            stat.Width = 120;
+            //stat.BackColor = Color.Red;
+            stat.ForeColor = Color.White;
+            stat.Location = new Point(550, 300);
+            stat.Text = "Statisztika";
+            stat.Name = "statButton";
+            stat.Font = new Font(betuTipus.Families[0], 14, FontStyle.Regular);
+            stat.Click += new EventHandler(statButton_Click);
+            Controls.Add(stat);
+
+        }
+
+        private void foglalasButton_Click(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Visible = true;
+
+            label2.Font = new Font("Microsoft YaHei UI", 10, FontStyle.Bold);
+            label3.Font = new Font("Microsoft YaHei UI", 10, FontStyle.Regular);
+
+        }
+
+        private void statButton_Click(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Visible = true;
+            tableLayoutPanel4.Visible = false;
+
+            label2.Font = new Font("Microsoft YaHei UI", 10, FontStyle.Regular);
+            label3.Font = new Font("Microsoft YaHei UI", 10, FontStyle.Bold);
 
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
-            tableLayoutPanel4.Visible = true;
+           // tableLayoutPanel4.Visible = true;
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
-           
+            
         }
     }
 }
